fe44fa4181ba751eafd54bb1861261350ff93ddd
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-02-21 14:34:21 -0600

    updating

--- src/main/java/com/codeup/codeupspringblog/MathController.java
+++ src/main/java/com/codeup/codeupspringblog/MathController.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MathController {
-    @GetMapping("/add/{num}/{number}")
+    @GetMapping("/add/{num}/and/{number}")
     @ResponseBody
     public String addNum(@PathVariable int num, @PathVariable int number) {
         return num + " plus " + number + " is " + (num + number);
     }
 
-    @GetMapping("/subtract/{num}/{number}")
+    @GetMapping("/subtract/{number}/from/{num}")
     @ResponseBody
     public String subtractNum(@PathVariable int num, @PathVariable int number) {
-        return num + " minus " + number + " is " + (number - num);
+        return num + " minus " + number + " is " + (num- number);
     }
 
-    @GetMapping("/multiply/{num}/{number}")
+    @GetMapping("/multiply/{num}/and/{number}")
     @ResponseBody
     public String multiplyNum(@PathVariable int num, @PathVariable int number) {
         return num + " times " + number + " is " + (number * num);
     }
 
 
-    @GetMapping("/divide/{num}/{number}")
+    @GetMapping("/divide/{num}/by/{number}")
     @ResponseBody
     public String divideNum(@PathVariable int num, @PathVariable int number) {
         return number + " divided by " + num + " is " + (number / num);
--- src/main/java/com/codeup/codeupspringblog/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/PostController.java
@@ -3,6 +3,7 @@ package com.codeup.codeupspringblog;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -26,7 +27,7 @@ public class PostController {
       return "This will be the page to show the form for creating a post";
     }
 
-    @GetMapping("/posts/create")
+    @PostMapping("/posts/create")
     @ResponseBody
     public String createPost(){
         return "This will be the post page that creates the new post" ;
