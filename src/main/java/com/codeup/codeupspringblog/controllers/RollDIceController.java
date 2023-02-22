5db22dc3caa1c56995c80feebcf95a578502c805
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-02-22 16:59:59 -0600

    updating dice game

--- src/main/java/com/codeup/codeupspringblog/controllers/RollDIceController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/RollDIceController.java
@@ -10,15 +10,25 @@ public class RollDIceController {
     @GetMapping("/roll-dice/{guess}")
     public String getDice(@PathVariable int guess, Model model) {
         String message;
-        int diceRoll = (int) Math.ceil(Math.random() * 6);
-        if (guess == diceRoll) {
-            message = "Good job! You guessed right!";
-        } else {
-            message = "Better luck next time...";
+        int diceRoll1 = (int) Math.ceil(Math.random() * 6);
+        int diceRoll2 = (int) Math.ceil(Math.random() * 6);
+        int diceTotal = diceRoll1 + diceRoll2;
+        if (guess == diceRoll1 && guess == diceRoll2 ) {
+            message = "WOW you guessed both dice right!";
+        } else if (guess == diceRoll1){
+            message = "Good job you guessed dice 1 right";
+        } else if (guess == diceRoll2) {
+            message = "Good job you guessed dice 2 right";
+        } else if (guess == diceTotal) {
+            message = "Your total for both dice equal your guess";
+        }else {
+            message = "Better luck next time....";
         }
         model.addAttribute("message", message);
         model.addAttribute("guess", guess);
-        model.addAttribute("diceRoll", diceRoll);
+        model.addAttribute("diceRoll1", diceRoll1);
+        model.addAttribute("diceRoll2", diceRoll2);
+        model.addAttribute("diceTotal",diceTotal);
         return "rolldice";
     }
 }
